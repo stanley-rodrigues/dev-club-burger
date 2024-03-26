c185853d5a3ed4e900eb7bba663cb482df84f38d
lucasisnardi <lucasisnardi08@gmail.com>
2024-03-25 21:51:54 -0300

    alterou

--- src/app/controllers/UserController.js
+++ src/app/controllers/UserController.js
@@ -3,26 +3,20 @@ import * as Yup from "yup"
 
 import User from "../models/User"
 
-class UserController{
-async store(request,response){
-    const schema = Yup.object().shape({
-        name: Yup.string().required(),
-        email: Yup.string().email().required(),
-        password: Yup.string().required().min(6),
-        admin: Yup.boolean(),
-    })
-        // if(!(await schema.isValid(request.body))){
-        //     return response
-        //     .status(400)
-        //     .json({error: "Make sure your data is correct"})
-        // }
-
+class UserController {
+    async store(request, response) {
+        const schema = Yup.object().shape({
+            name: Yup.string().required(),
+            email: Yup.string().email().required(),
+            password: Yup.string().required().min(6),
+            admin: Yup.boolean(),
+        })
         try {
-            await schema.validateSync(request.body, { abortEarly: false})
-        }  catch (err) {
-            return response.status(400).json({error: err.errors})
+            await schema.validateSync(request.body, { abortEarly: false })
+        } catch (err) {
+            return response.status(400).json({ error: err.errors })
         }
-           
+
         const { name, email, password, admin } = request.body
 
         const userExists = await User.findOne({
@@ -30,21 +24,21 @@ async store(request,response){
         })
 
         if (userExists) {
-            return response.status(400).json({error: "User already exists"})
+            return response.status(400).json({ error: "User already exists" })
         }
 
         console.log(userExists)
-      
-        const user = await User.create({    
+
+        const user = await User.create({
             id: v4(),
-            name, 
-            email, 
-            password, 
+            name,
+            email,
+            password,
             admin,
 
-        })  
+        })
 
-       return response.status(201).json({ id: user.id, name, email, admin})
+        return response.status(201).json({ id: user.id, name, email, admin })
     }
 }
 
